0f8b953d877d18c44aa5c3d6ddc58efcf309c687
Guga <gugatascheri@gmail.com>
2023-12-09 19:14:52 -0300

    Atualizacao

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: 'app-bolos',
+  templateUrl: 'bolos.page.html',
+  styleUrls: ['bolos.page.scss'],
 })
-export class Tab2Page {
-
-  constructor() {}
-
+export class BolosPage {
+  bolos: any[] = [
+    {
+      nome: 'Bolo de Chocolate',
+      imagem: 'caminho-para-imagem-1.jpg',
+      ingredientes: 'Chocolate, farinha, ovos, açúcar',
+      preco: 29.99,
+    },
+    // ... (repita para cada tipo de bolo)
+    {
+      nome: 'Bolo de Morango',
+      imagem: 'caminho-para-imagem-30.jpg',
+      ingredientes: 'Morangos frescos, farinha, ovos, açúcar',
+      preco: 34.99,
+    },
+  ];
 }
