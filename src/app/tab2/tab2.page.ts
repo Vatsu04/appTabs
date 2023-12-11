09c14e213c468e898c07c2eaecdf8fdb3b53fe84
Guga <gugatascheri@gmail.com>
2023-12-10 21:44:11 -0300

    Atualiza;'oes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,15 +10,30 @@ export class BolosPage {
     {
       nome: 'Bolo de Chocolate',
       imagem: 'caminho-para-imagem-1.jpg',
-      ingredientes: 'Chocolate, farinha, ovos, açúcar',
+      ingredientes: 'Chocolate, farinha, ovos, açúcar, Whey de Chocolate',
       preco: 29.99,
     },
     // ... (repita para cada tipo de bolo)
     {
       nome: 'Bolo de Morango',
       imagem: 'caminho-para-imagem-30.jpg',
-      ingredientes: 'Morangos frescos, farinha, ovos, açúcar',
+      ingredientes: 'Morangos frescos, farinha, ovos, açúcar, Whey de Morango',
       preco: 34.99,
     },
+  {
+    nome:'Bolo de Baunilha',
+    imagem: 'caminho-para-imagem-30.jpg',
+    ingredientes: 'Essência de baunilha, farinha, ovos, açúcar, Whey de Baunilha'
+  },
+  {
+    nome:'Pamonha',
+    imagem:'caminho-para-imagem-30.jpg',
+    ingredientes:'Milho, farinha, ovos, açúcar, Whey de Pamonha'
+  },
+  {
+    nome:'Bolo de Chocolate Vegano',
+    imagem:'caminho-para-imagem-30.jpg',
+    ingredientes:'Chocolate vegano, farinha, linhaça, açúcar, Whey de Chocolate Vegano'
+  }
   ];
 }
